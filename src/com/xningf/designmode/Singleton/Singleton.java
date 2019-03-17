d3bc21affd52e8d112bff3b34536dcaa674534c9
xning <48199085+wuwangxn@users.noreply.github.com>
2019-03-17 21:38:37 +0800

    Update Singleton.java

--- src/com/xningf/designmode/Singleton/Singleton.java
+++ src/com/xningf/designmode/Singleton/Singleton.java
@@ -4,7 +4,7 @@ import com.sun.deploy.util.StringUtils;
 
 public class Singleton {
 
-    static class sing{
+    static class sing{//静态内部类
         public static String singleton = "gingleton";
     }
 
